87178cb0607bf0a5de70640e2ca883ee146b0328
Alexandre-Alberti <125623015+Alexandre-Alberti@users.noreply.github.com>
2025-03-21 17:31:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -4,8 +4,9 @@ Created on Fri Mar 21 16:18:18 2025
 
 @author: alexa
 """
-
 import streamlit as st
+import numpy as np
+from scipy.optimize import differential_evolution
 
 def parse_input(input_str):
     """Converte a string de entrada em uma lista de números."""
@@ -14,28 +15,56 @@ def parse_input(input_str):
     except ValueError:
         return []
 
+#definição das funções da distribuição de probabilidade
+def f(t,beta,eta):
+    return (beta/eta)*((t/eta)**(beta-1))*np.exp(-((t/eta)**beta))
+
+def F(t,beta,eta):
+    return 1 - np.exp(-((t/eta)**beta))
+
+def R(t,beta,eta):
+    return np.exp(-((t/eta)**beta))
+
+#função de verossimilhança
+def V_neg(parametros, DCp, DCE, DCD):
+    beta = parametros[0]
+    eta = parametros[1]
+    Vs = 1
+    if len(DCp)>0:
+        for i in range(0,len(DCp)):
+            Vs = Vs*f(DCp[i],beta,eta)
+    if len(DCE)>0:
+        for j in range(0,len(DCE)):
+            Vs = Vs*F(DCE[j],beta,eta)
+    if len(DCD)>0:
+        for k in range(0,len(DCD)):
+            Vs = Vs*R(DCD[k],beta,eta)
+    return -Vs
+
 # Configuração da interface
-st.title("Soma de Vetores")
+st.title("Ajuste para a Distribuição Weibull")
 
 # Campos de entrada para os vetores
-A_str = st.text_input("Insira os números do vetor A, separados por vírgula:")
-B_str = st.text_input("Insira os números do vetor B, separados por vírgula:")
-C_str = st.text_input("Insira os números do vetor C, separados por vírgula:")
+DCp_str = st.text_input("DADOS COMPLETOS - insira os dados de tempo até a falha, separados por vírgula:")
+DCE_str = st.text_input("DADOS CENSURADOS À ESQUERDA - insira os dados de quando as falhas foram observadas, separados por vírgula:")
+DCD_str = st.text_input("DADOS CENSURADOS À DIREIRA - insira os dados do tempo de funcionamento das unidades que não falharam, separados por vírgula:")
 
-if st.button("Somar"):
+
+if st.button("Estimar parâmetros:"):
     # Converter entradas para listas numéricas
-    A = parse_input(A_str)
-    B = parse_input(B_str)
-    C = parse_input(C_str)
+    DCp = parse_input(DCp_str)
+    DCE = parse_input(DCE_str)
+    DCD = parse_input(DCD_str)
     
-    # Somar os elementos dos vetores
-    A_soma = sum(A)
-    B_soma = sum(B)
-    C_soma = sum(C)
-    soma_total = A_soma + B_soma + C_soma
+    max_eta = 100*max(max(DCp),max(DCE),max(DCD))
+    
+    #minimizando V_neg (maximizando V)
+    bounds = [(0,20), (0,max_eta)]
+    res = differential_evolution(lambda x: V_neg(x, DCp, DCE, DCD),bounds)
+    
+    beta_estimado = res.x[0]
+    eta_estimado = res.x[1]
     
     # Exibir resultados
-    st.write(f"A_soma: {A_soma}")
-    st.write(f"B_soma: {B_soma}")
-    st.write(f"C_soma: {C_soma}")
-    st.write(f"Soma Total: {soma_total}")
+    st.write(f"Parâmetro de forma: {beta_estimado}")
+    st.write(f"Parâmetro de escala: {eta_estimado}")
